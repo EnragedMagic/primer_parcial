7a0d086d54966b3a92f0da7a8ba2fe381694db4d
Johan Galeano <104652500+EnragedMagic@users.noreply.github.com>
2025-09-05 07:38:56 -0500

    Create primer_punto.hs

---
+++ primer_punto.hs
@@ -0,0 +1,18 @@
+
+-- ordenar.hs
+module Main where
+
+ordenar :: Ord a => [a] -> [a]
+ordenar []     = []
+ordenar (x:xs) = insertar x (ordenar xs)
+
+insertar :: Ord a => a -> [a] -> [a]
+insertar y [] = [y]
+insertar y (z:zs)
+  | y <= z    = y : z : zs
+  | otherwise = z : insertar y zs
+
+main :: IO ()
+main = do
+  let calificaciones = [4.5, 3.2, 5.0, 2.8, 4.0]
+  print (ordenar calificaciones)
